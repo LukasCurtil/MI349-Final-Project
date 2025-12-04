7ea95f395a305f22994d9cd9bf7a54fc4ff5abdb
LukasCurtil <curtillu@msu.edu>
2025-12-04 02:21:40 +0000

    Added a metronome to the site, a tip of the day function, and an alert when contact form is submitted.

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function() {
-    alert("Thank you for sending a message! I will get back to you within 24-72 hours!");
-    this.reset();
-  });
